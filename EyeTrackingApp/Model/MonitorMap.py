645c074eabbb95da83e40de8464f212294d31956
Gabriel Genest <9.genest.gabriel@gmail.com>
2025-10-13 15:38:56 -0400

    Ajout de méthodes pour fit un modèle linéaire
    On fit un modèle linéaire polynomial + Ridge

--- EyeTrackingApp/Model/MonitorMap.py
+++ EyeTrackingApp/Model/MonitorMap.py
@@ -1,25 +1,82 @@
 from dataclasses import dataclass
 import numpy as np
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.pipeline import Pipeline
+
+import matplotlib
+
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
 
 @dataclass
 class LineOfSight:
-	x: float # pixel
-	y: float # pixel
-	theta_x: float # degrés
-	theta_y: float # degrés
+    x: float  # pixel
+    y: float  # pixel
+    theta_x: float  # degrés
+    theta_y: float  # degrés
+
 
 class MonitorMap:
-	def __init__(self, screen_width, screen_height):
-		self.w = screen_width
-		self.h = screen_height
+    def __init__(self, screen_width, screen_height):
+        self.w = screen_width
+        self.h = screen_height
+        self.x_screen = []
+        self.y_screen = []
+        self.data = []
+        self.model_x = None
+        self.model_y = None
+        self.feature_mat = None
 
-	def set_new_point(self, point_id, left_vectors, right_vectors):
-		""" ici left_vectors et right_vectors sont des listes de LineOfSight
-			point_id est un int de 0 à 8, tu peux retrouver la ligne + colonne 
-			avec point_i//3, point_id%3). cela représente la position relative
-			sur ton écran, de haut en bas.
-		""" 
-		pass
+    def set_new_point(self, point_id, left_vectors, right_vectors):
+        """ ici left_vectors et right_vectors sont des listes de LineOfSight
+            point_id est un int de 0 à 8, tu peux retrouver la ligne + colonne
+            avec point_i//3, point_id%3). cela représente la position relative
+            sur ton écran, de haut en bas.
+        """
 
-	def translate_to_monitor_position(self, left_vector, right_vector):
-		return np.array([y, x])
+        x_point = (point_id // 3) * self.w
+        y_point = (point_id % 3) * self.h
+        nframes = len(right_vectors)
+        self.x_screen.extend([x_point] * nframes)
+        self.y_screen.extend([y_point] * nframes)
+        data = np.zeros((len(right_vectors), 8))
+        for i in range(nframes):
+            l = left_vectors[i]
+            r = right_vectors[i]
+            data[i] = l.x, l.y, l.theta_x, l.theta_y, r.x, r.y, r.theta_x, r.theta_y
+        self.data.append(data)
+
+    def feature_maxtrix(self):
+        return np.vstack(self.data)
+
+    def train_model(self, polynomial_degree=2, alpha=1):
+        X = self.feature_maxtrix()
+        poly = PolynomialFeatures(degree=polynomial_degree, include_bias=True)
+        scaler = StandardScaler()
+
+        pipeline_x = Pipeline([("scaler", scaler), ("poly", poly), ("ridge", Ridge(alpha=alpha))])
+        pipeline_y = Pipeline([("scaler", scaler), ("poly", poly), ("ridge", Ridge(alpha=alpha))])
+        pipeline_x.fit(X, self.x_screen)
+        pipeline_y.fit(X, self.y_screen)
+        self.model_x, self.model_y = pipeline_x, pipeline_y
+
+    def predict(self, left_vector: LineOfSight, right_vector: LineOfSight):
+        if self.model_x is None or self.model_y is None:
+            raise ValueError("Please train the model before predicting.")
+        xl = left_vector.x
+        yl = left_vector.y
+        txl = left_vector.theta_x
+        tyl = left_vector.theta_y
+        xr = right_vector.x
+        yr = right_vector.y
+        txr = right_vector.theta_x
+        tyr = right_vector.theta_y
+        X = np.array([xl, yl, txl, tyl, xr, yr, txr, tyr]).reshape(1, -1)
+        pred_x = self.model_x.predict(X)
+        pred_y = self.model_y.predict(X)
+        return np.array([pred_x, pred_y])
+
+    def translate_to_monitor_position(self, left_vector, right_vector):
+        return self.predict(left_vector, right_vector)
